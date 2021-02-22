225c7c979aa465a292f8b65997bab1940a95e43d
Nessun Kim <nessunkim@gmail.com>
2021-02-23 01:40:35 +0900

    Fix attribution

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -49,7 +49,7 @@ const useMap = () => {
     map = L.map("map").setView([37.7, 126.7], 7);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | &copy; <a href="https://librewiki.net">Libre Wiki</a> contributors',
     }).addTo(map);
     await updateMarkers();
     map.on("contextmenu", (e) => {
