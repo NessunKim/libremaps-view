a40343b37cc7d3d281d7da8eb9b0ba822ef29b51
Nessun Kim <nessunkim@gmail.com>
2021-02-24 03:19:49 +0900

    Fix style

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -202,6 +202,6 @@ export default {
 }
 
 .new-marker-submit {
-  @apply block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center outline-none;
+  @apply block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center outline-none w-full;
 }
 </style>
